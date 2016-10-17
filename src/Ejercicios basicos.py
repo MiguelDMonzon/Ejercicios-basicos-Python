b1572cff62625267c7b9376a8e4c2d5eafd2872b
Unknown <migueldmonzon@gmail.com>
2016-10-17 19:20:11 +0100

    V0.3

--- src/Ejercicios basicos.py
+++ src/Ejercicios basicos.py
@@ -4,10 +4,13 @@ from math import pi
 
 # Funcion que retorne el mayor de dos numeros o 0 si son iguales
 def max_numeros(num1, num2):
-    """ float, float -> float
-        OBJ: Retorna el mayor de dos números x e y
     """
-    
+    Devuelve el mayor de dos numeros x e y
+    :param num1: float
+    :param num2: float
+    :return: float
+    """
+
     if num1 > num2:
         return num1
     elif num2 > num1:
@@ -17,25 +20,27 @@ def max_numeros(num1, num2):
 
 
 # Funcion que determine si una persona es mayor de edad o no
-def es_mayor_edad():
-    edad = input("Inserte la edad: ")
+def es_mayor_edad(edad):
+    """
+    Determina si una persona es mayor de edad a partir de su edad
+    :param edad: int
+    :return: bool
+    """
     return edad >= 18
 
 
 # Programar una funcion que determine si una empresa es microempresa o no (retorno booleano True o False).
 # Se dice que es microempresa si tiene menos de 50 empleados, factura menos de 30
 # millones de euros y tiene un balance igual o inferior a los 5 millones de euros.
-def es_microempresa():
-    num_empleados = input("Inserte el numero de empleados: ")
-    if num_empleados < 50:
-        facturacion = input("Inserte la facturacion (en millones de euros): ")
-        if facturacion < 30:
-            balance = input("Ahora, inserte el balance (en millones de euros): ")
-            return balance <= 5
-        else:
-            return False
-    else:
-        return False
+def es_microempresa(num_empleados, facturacion, balance):
+    """
+    Determina si una empresa es microempresa o no
+    :param num_empleados: int
+    :param facturacion: int
+    :param balance: int
+    :return: bool
+    """
+    return num_empleados < 50 and facturacion < 30 and balance <= 5
 
 
 # Calcular el impuesto que debe pagar un contribuyente a partir de sus ingresos anuales
@@ -43,6 +48,11 @@ def es_microempresa():
 # siendo este ultimo igual a los ingresos totales menos una deduccion personal de 600 euros
 # y otra deduccion de 60 euros por hijo.
 def calcular_impuesto():
+    """
+    Calcula el impuesto que debe pagar un contribuyente en funcion se sus ingresos anuales y el
+    numero de hijos.
+    :return: float
+    """
     ingresos_anuales = input("Inserte sus ingresos anuales (en euros): ")
     tiene_hijos = input("Tiene Ud. hijo/as? (S/N): ")
     if tiene_hijos == "S":
@@ -50,14 +60,13 @@ def calcular_impuesto():
         ingreso_imponible = ingresos_anuales - 600 - (60 * num_hijos)
     else:
         ingreso_imponible = ingresos_anuales - 600
-    return ingreso_imponible / 3
+    return ingreso_imponible / 3.0
 
 # Hacer los calculos que permiten saber si una empresa de 20 empleados, 18 millones de euros de
 # facturacion y 5 millones de euros de balance es una micro empresa y almacenar el
 # valor en una variable logica (booleana).
 # Solucion: llamar a la funcion, y escribir 20, 18 y 5 => True
-ejercicio_microempresa = es_microempresa()
-print(str(ejercicio_microempresa))
+ejercicio_microempresa = es_microempresa(20, 18, 5)
 
 
 # La temperatura expresada en grados centigrados TC, se puede convertir a grados
@@ -65,20 +74,33 @@ print(str(ejercicio_microempresa))
 # para hacer esta transformacion que reciba como argumento la temperatura en grados
 # centigrados y retorne su equivalente en Farenheit.
 def centigrados_to_fahrenheit(grados_centigrados):
-    return 9*grados_centigrados/5 + 32
+    """
+    Transforma grados centigrados a fahrenheit
+    :param grados_centigrados: float
+    :return: float
+    """
+    return 9*grados_centigrados/5.0 + 32
 
 
 # Escribir una funcion Python que a partir de una cierta cantidad en euros y
 # del tipo de cambio del dia, retorne el equivalente en libras teniendo en cuenta que
 # la casa de cambio retiene una comision del 2% sobre el total de la operacion.
-def euros_to_libras():
-    euros = input("Escriba el numero de euros a convertir: ")
-    tipo = input("Escriba el tipo de cambio del dia: ")
+def euros_to_libras(euros, tipo):
+    """
+    Transforma euros a libras, con una comision del 2%
+    :param euros: float
+    :param tipo: float
+    :return: float
+    """
     return (euros*tipo)*0.98
 
 
 # Procedimiento que pinte una linea de asteriscos en pantalla
 def pintar_asteriscos():
+    """
+    Pinta una linea de asteriscos
+    :return: void
+    """
     print("********************************")
 
 
@@ -88,9 +110,13 @@ def pintar_asteriscos():
 # Autor: Miguel David Monzon
 # Email: miguel.monzon@uah.es
 # **************************************
-def pintar_banner():
-    autor = input("Inserte su nombre: ")
-    email = input("Inserte su email: ")
+def pintar_banner(autor, email):
+    """
+    Pinta un banner con autor junto a su e-mail
+    :param autor: string
+    :param email: string
+    :return: void
+    """
     pintar_asteriscos()
     print("Autor: "+autor)
     print("Email: "+email)
@@ -100,23 +126,41 @@ def pintar_banner():
 # Programa modularizado que, utilizando funciones, calcule el perimetro y el area de
 # un circulo cuyo radio es proporcionado por el usuario
 def calcular_perimetro(radio):
+    """
+    Calcula el perimetro de una circunferencia dado su radio
+    :param radio: float
+    :return: float
+    """
     return 2 * pi * radio
 
 
 def calcular_area(radio):
-    return pi * (r ** 2)
+    """
+    Calcula el area de una circunferencia dado su radio
+    :param radio: float
+    :return: float
+    """
+    return pi * (radio ** 2)
 
 
 def programa_calculo():
+    """
+    Calcula el perimetro y el area de una circunferencia dado su radio
+    :return: void
+    """
     radio = input("Inserte el radio del circulo: ")
-    print("Su perimetro es: "+ str(calcular_perimetro(radio)))
-    print("Su area es: "+str(calcular_area(radio)))
+    print("Su perimetro es: " + str(calcular_perimetro(radio)))
+    print("Su area es: " + str(calcular_area(radio)))
 
 
 # Escribir un programa para solicitar al usuario el numero de horas y el precio por hora
 # con vistas a calcular su salario bruto. Las horas que sobrepasen 40 se consideraran
 # extra y pagadas a 1,5 veces el precio de la hora regular.
 def calcular_salario_bruto():
+    """
+    Calcula salario bruto en funcion del numero de horas y el precio de la hora regular y extra
+    :return: float
+    """
     num_horas = input("Inserte el numero de horas: ")
     precio_hora = input("Inserte el precio de la hora (regular): ")
     if num_horas > 40:
@@ -130,20 +174,33 @@ def calcular_salario_bruto():
 # Programar una funcion que determine si un numero es par o impar. La funcion debe retornar
 # verdadero o falso haciendo uso de valores booleanos.
 def es_par(num):
+    """
+    Devuelve si un numero es par o no
+    :param num: int
+    :return: bool
+    """
     return num % 2 == 0
 
 
 # Programar una funcion que determine si una letra es vocal o no.
 def es_vocal(letra):
-    # El ASCII de la A a la Z es de 65 a 90, y de a - z es de 97 a 122
-    ascii_letra = ord(letra)
-    return (ascii_letra >= 65 and ascii_letra <= 90) or (ascii_letra >= 97 and ascii_letra <= 122)
+    """
+    Devuelve si una letra es vocal o no
+    :param letra: char
+    :return: bool
+    """
+    return letra in ['a', 'e', 'i', 'o', 'u']
 
 
 # Programar una funcion en Python que a partir de un numero entero entre 1 y 7 que recibe como
 # argumento retorne el dia de la semana que corresponda, y un mensaje de error si
 # el numero no esta entre 1 y 7.
 def dia_semana(num):
+    """
+    Devuelve el dia de la semana correspondiente al numero pasado por parametro
+    :param num: int
+    :return: string
+    """
     if num == 1:
         return "Lunes"
     elif num == 2:
@@ -163,6 +220,11 @@ def dia_semana(num):
 
 
 def dia_semana_switch(num):
+    """
+    Devuelve el dia de la semana correspondiente al numero pasado por parametro, usando switch
+    :param num: int
+    :return: string
+    """
     switcher = {
         1: "Lunes",
         2: "Martes",
